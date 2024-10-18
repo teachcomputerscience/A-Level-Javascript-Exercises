ad901aef9eda071ada03ad8295046c5ce255b4df
teachcomputerscience <100346416+teachcomputerscience@users.noreply.github.com>
2024-10-18 12:31:40 +0100

    updating exercise 1

--- Javascript Arrays/Exercise 1/script.js
+++ Javascript Arrays/Exercise 1/script.js
@@ -1,14 +1,47 @@
 //declare array for values to be stored
-const valueArray = [];
+let valueArray = [];
+let modalDisplay = false;
+let output = document.getElementById('output');
 
 let btnAddValue = document.getElementById('btnAddToArray');
 let btnRemoveValue = document.getElementById('btnRemoveFromArray');
 let btnSortArray = document.getElementById('btnSortArray');
 let btnDisplayArray = document.getElementById('btnDisplayArray');
 let modalInput = document.getElementById('modalInput');
+let overlay = document.getElementById('overlay');
 
-btnAddValue.addEventListener('click', addValueToArray);
+btnDisplayArray.addEventListener('click', displayArray);
+btnAddValue.addEventListener('click', displayModal);
+overlay.addEventListener('click', hideModal);
 
 function addValueToArray() {
+    let valueToAdd = document.getElementById('valueToAdd').value;
+    alert("added");
+    valueArray.push(valueToAdd);
+}
+
+
+function displayModal() {
+    if (!modalDisplay) {
     modalInput.style.display ="block";
+    overlay.style.display = "block";
+    modalDisplay = true;
+    let btnAdd = document.getElementById('btnAddValue');
+    btnAdd.addEventListener('click', addValueToArray);
+    }
+}
+
+function hideModal() {
+    if (modalDisplay) {
+    modalInput.style.display = "none";
+    overlay.style.display = "none";
+    modalDisplay = false;
+    }
+}
+
+function displayArray() {
+    output.innerHTML = 'grgegreg'
+    for (let i = 0; i < valueArray.length; i++) {
+        output.innerHTML = output.innerHTML + '<br>' + valueArray[i];
+    }
 }
