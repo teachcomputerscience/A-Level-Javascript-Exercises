0e86eae2e6e1f2f9f06fa5cea6e277f6bb0e8f82
teachcomputerscience <100346416+teachcomputerscience@users.noreply.github.com>
2025-01-14 15:41:56 +0000

    updated to test codespace

--- Class Exercises/1 - Introduction to Javascript/Exercise 1/script.js
+++ Class Exercises/1 - Introduction to Javascript/Exercise 1/script.js
@@ -2,7 +2,7 @@
 function addNumbers() {
     let num1 = parseFloat(document.getElementById('number1').value);
     let num2 = parseFloat(document.getElementById('number2').value);
-
+    //validate and complete sum
     if (isNaN(num1) || isNaN(num2)) {
         document.getElementById('result').textContent = "Please enter valid numbers!";
     } else {
