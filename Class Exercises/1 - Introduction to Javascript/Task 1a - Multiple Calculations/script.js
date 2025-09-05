31363497c70b2db9486d3a25fdd71dde18e7412f
Euan-Crit <627ec83@canonslade.tbft.uk>
2025-09-05 12:08:07 +0000

    complete ex 1

--- Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1a - Multiple Calculations/script.js
@@ -4,9 +4,19 @@ document.getElementById('calculateBtn').addEventListener('click', calculate);
 function calculate() {
     // TODO:
     // 1. Get the three numbers (A, B, C) from the input fields
+    let num1 = parseFloat(document.getElementById('numberA').value);
+    let num2 = parseFloat(document.getElementById('numberB').value);
+    let num3 = parseFloat(document.getElementById('numberC').value); 
     // 2. Calculate:
     //    - (A + B + C) / 3
+    const sum1 = (num1 + num2 + num3)/3
     //    - A × B × C
+    const sum2 = num1 * num2 * num3
     //    - A + (B × C)
+    const sum3 = num1 + (num2 *num3)
     // 3. Display the results in the respective span elements
+
+    document.getElementById('average').textContent = `The average is  ${sum1}`
+    document.getElementById('product').textContent = `the product is  ${sum2}`
+    document.getElementById('expression').textContent = `the expression is  ${sum3}` 
 }
--- Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
@@ -4,9 +4,14 @@ document.getElementById('calculateBtn').addEventListener('click', calculateCircl
 function calculateCircle() {
     // TODO:
     // 1. Get the radius value from the input field
+    let rad = parseFloat(document.getElementById('radius').value);
     // 2. Calculate:
     //    - Circumference using the formula: 2 * π * radius
+    let cir = rad * 2 * Math.PI
     //    - Area using the formula: π * radius^2
+    let area = (rad**2) * Math.PI
     // 3. Display both results in their respective span elements
+    document.getElementById('circumference').textContent = `The circumference is  ${cir}`
+    document.getElementById('area').textContent = `the area is  ${area}`
     // Note: Use Math.PI for the value of π
 }
