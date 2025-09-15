4bc21293c01db9cebbaf9d506918c0564c87673d
VictoriaICHELLA <627ii60@canonslade.tbft.uk>
2025-09-15 11:06:10 +0000

    /commit

--- Class Exercises/2 - Javascript Selection/Task 2b - Vocational Grade/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2b - Vocational Grade/script.js
@@ -10,22 +10,31 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the Hang Gliding Certificate grade
 function calculateGrade() {
 // TODO: Get the theory and practical exam scores
-    let theory = document.getElementById('theory').value
-    let prati = document.getElementById('practical').value
+    let theory = parseFloat(document.getElementById('theory').value)
+    let practi = parseFloat(document.getElementById('practical').value)
 
 // TODO: Check if both scores are above 50% (Pass requirement)
+    let aver = (theory + practi)/ 2
+    let message = ""
+
     if (theory >= 50 && practi >= 50){
 
 // TODO: If passed, calculate average and check if above 70% (Distinction requirement)
-        aver = (theory + practi)/ 2
+       
         if (aver > 70) {
             message = "Distinction"
+        } else{
+            message = "Pass"
         }
-        
+    } else{ 
+        message = "Fail"
+    }
+
     // TODO: Display the appropriate grade (Pass, Distinction, or Fail)
-
-
+    document.getElementById('result').innerText = message
     // Also display the average score
+    document.getElementById('average').innerText = aver + "%"
+    
 
 
 }
--- Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
@@ -10,22 +10,43 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the insurance premium
 function calculatePremium() {
     // TODO: Get values from input fields
+    let ager  = number(document.getElementID(age).value)
+    let years = number(document.getElementID(noClaims).value)
+    let PC = number(document.getElementID(previousClaims).value)
     
     // TODO: Calculate base premium (£500)
-    
+    let basepre = 500
+    let add = 0
     // TODO: Apply age adjustment
     // Under 25: +50%
+    if  (ager < 25){]
+        add = basepre * 0.5
+        basepre = add + basepre
+    }else if (ager >= 40){
+        add = basepre * 1.15
+        basepre = 
+    }
     // 25-40: No change
+
     // Over 40: -15%
     
+
     // TODO: Apply no claims bonus
+
     // 0 years: No discount
+
     // 1-2 years: 10% discount
+
     // 3-5 years: 25% discount
+
     // Over 5 years: 35% discount
+
     
     // TODO: Add previous claims adjustment
+
     // Each claim adds 20%
+
     
     // TODO: Display the final premium and the breakdown of calculations
+
 }
