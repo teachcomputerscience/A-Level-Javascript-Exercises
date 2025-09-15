8391ba6767d9eed8959d8ff8e35fe426d76de2ad
Euan-Crit <627ec83@canonslade.tbft.uk>
2025-09-15 10:16:02 +0000

    Fix: Update live server port and enhance car insurance premium calculation logic

--- Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
@@ -10,22 +10,40 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the insurance premium
 function calculatePremium() {
     // TODO: Get values from input fields
-    
+    let age = parseFloat(document.getElementById('age').value)
+    let noclaims = parseFloat(document.getElementById('noClaims').value)
+    let prev = parseFloat(document.getElementById('previousClaims').value)
     // TODO: Calculate base premium (£500)
+    let base = 500
     
     // TODO: Apply age adjustment
+    if (age <= 25) {
+        base = base * 2
+    } else if ( age > 40) {
+        base = base * 0.85
+    } 
     // Under 25: +50%
     // 25-40: No change
     // Over 40: -15%
     
     // TODO: Apply no claims bonus
+    if (noclaims >= 1 && noclaims < 3) {
+      base = base * 0.9
+    } else if (noclaims >= 3 && noclaims < 6){
+        base = base * 0.75
+    } else if ( noclaims > 5 ) {
+        base = base * 0.65
+    }
+
     // 0 years: No discount
     // 1-2 years: 10% discount
     // 3-5 years: 25% discount
     // Over 5 years: 35% discount
     
     // TODO: Add previous claims adjustment
+    claimajst = (prev * 0.2) + 1
     // Each claim adds 20%
-    
+    base = base * claimajst
     // TODO: Display the final premium and the breakdown of calculations
+    document.getElementById('result').textContent = `premuim = ${base} `
 }
