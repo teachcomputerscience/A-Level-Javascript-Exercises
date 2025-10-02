0145c5e5a9d08f95ac1472bbc858ef94f51b3584
IsaacWane <627iw13@canonslade.tbft.uk>
2025-10-02 16:53:52 +0000

    stuff

--- Class Exercises/4 - Javascript Iteration Using While/Task 1 Example - Basic While/script.js
+++ Class Exercises/4 - Javascript Iteration Using While/Task 1 Example - Basic While/script.js
@@ -1,14 +1,7 @@
 // Add your event listener and while loop code here
-document.getElementById("showNumbersBtn").addEventListener("click",function ()  {
-       Numbers1To10() });
-      
-       let i = 1
-      let numbers1to10 = []
-      while(i <= 10){
-       numbers1to10.push(i);
-       i++
+
         
-      }
+      
        // When the button is clicked, display numbers 1 to 10 in the output area using a while loop
 
      
--- Class Exercises/4 - Javascript Iteration Using While/Task 1a - Count Up/script.js
+++ Class Exercises/4 - Javascript Iteration Using While/Task 1a - Count Up/script.js
@@ -1,4 +1,14 @@
 // Add your event listener and while loop code here
 
+    
+     
+let n = parseFloat(document.getElementById('num1').value);
+let i = 1;
+let output = '';
 
+while (i <= n) {
+      output += i + ' ';
+      i++; 
+}
+document.getElementById('output').textContent = output;
 // When the button is clicked, display numbers 1 to N in the output area using a while loop
