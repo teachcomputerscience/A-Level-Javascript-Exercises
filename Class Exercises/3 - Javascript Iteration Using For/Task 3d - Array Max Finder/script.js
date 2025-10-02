40d1faa9232397e6b335832c387b03731e446692
teachcomputerscience <100346416+teachcomputerscience@users.noreply.github.com>
2025-10-02 12:50:41 +0100

    Update script.js

--- Class Exercises/3 - Javascript Iteration Using For/Task 3d - Array Max Finder/script.js
+++ Class Exercises/3 - Javascript Iteration Using For/Task 3d - Array Max Finder/script.js
@@ -1,24 +1,14 @@
 let values = []
 
 function generateValues() {
-  for (i=0; i<=100; i++) {
-    values.push(Math.ceil(Math.random() * (1000-1) + 1));
-  }
+
 }
 
 
 function findHighest(values) {
-  let highest = 0
-  values.forEach(value => {
-    if (value > highest) {
-      highest = value;
-    }
-    document.getElementById("output").innerHTML = `Highest value is ${highest}`;
-  });
+
 }
 
 document.getElementById("btnFindMax").addEventListener("click", () => {
-  values = [];
-  generateValues();
-  findHighest(values);
+
 });
