055d2baabeeb81489d6234259a9513391bf401f3
Euan-Crit <627ec83@canonslade.tbft.uk>
2025-10-03 12:05:31 +0000

    Fix: Correct loop condition in random number generation and improve formatting

--- Class Exercises/3 - Javascript Iteration Using For/Task 3d - Array Max Finder/script.js
+++ Class Exercises/3 - Javascript Iteration Using For/Task 3d - Array Max Finder/script.js
@@ -1,8 +1,25 @@
 function getrandomnum(max) {
     return Math.floor(Math.random() * max);
 }
+
+
+document.getElementById("btnFindMax")
 let numbers = []
 let i = 0
-for (i = 0; i >= 10; i++) {
+for (i = 0; i <= 10; i++) {
     numbers(i) = getrandomnum(50)
 }
+
+
+
+function findmax(numbers) {
+    let max = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+document.getElementById('output').textContent = findmax(numbers);
+
