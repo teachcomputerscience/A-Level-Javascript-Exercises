fab660003e39bad61eebf9f2ae1dbd282475ee63
Euan-Crit <627ec83@canonslade.tbft.uk>
2025-10-03 11:33:31 +0000

    Refactor: Replace static number array with random number generation logic

--- Class Exercises/3 - Javascript Iteration Using For/Task 3d - Array Max Finder/script.js
+++ Class Exercises/3 - Javascript Iteration Using For/Task 3d - Array Max Finder/script.js
@@ -1,10 +1,8 @@
-const numbers = [12, 45, 7, 22, 99, 3, 18];
-document.getElementById("btnFindMax").onclick = function() {
-  let max = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > max) {
-      max = numbers[i];
-    }
-  }
-  document.getElementById("output").innerText = `The biggest number is: ${max}`;
-};
+function getrandomnum(max) {
+    return Math.floor(Math.random() * max);
+}
+let numbers = []
+let i = 0
+for (i = 0; i >= 10; i++) {
+    numbers(i) = getrandomnum(50)
+}
