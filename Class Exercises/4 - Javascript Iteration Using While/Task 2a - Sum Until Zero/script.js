e9d484c25d323f60f3cbb288bf40fcb2f7929270
IsaacWane <627iw13@canonslade.tbft.uk>
2025-10-07 08:50:12 +0000

    rttr

--- Class Exercises/4 - Javascript Iteration Using While/Task 2a - Sum Until Zero/script.js
+++ Class Exercises/4 - Javascript Iteration Using While/Task 2a - Sum Until Zero/script.js
@@ -1,14 +1,26 @@
 // Add your event listener and while loop code here
-document.getElementById('addBtn').addEventListener('click', function() {
-   
-    let sum = 0 
- while (num1 > 0){
+document.getElementById('addBtn').addEventListener('click', function () {
   let num1 = parseFloat(document.getElementById('numberInput').value);
-    sum = num1 + sum ;
-    i++;
-}
+ let i = 0 
+  
 
- document.getElementById('output').textContent = `The sum is: ${sum}`;
+  while (num1 !== 0) {
+let sum = i
+   
+    let num1 = parseFloat(document.getElementById('numberInput').value);
+    i = num1 + sum;
+
+    
+    
+    document.getElementById('output').textContent = `The current sum is: ${i}`;
+    return i;
+
+  }
+
+  document.getElementById('output').textContent = `The final sum is: ${i}`;
 
 });
+
+
+
 // When the button is clicked, use a while loop to sum numbers until 0 is entered
--- Class Exercises/4 - Javascript Iteration Using While/Task 2b - Guess the Number Game/script.js
+++ Class Exercises/4 - Javascript Iteration Using While/Task 2b - Guess the Number Game/script.js
@@ -1,2 +1,4 @@
 // Add your event listener and while loop code here
+
+
 // Generate a random number, then use a while loop to check guesses until correct
