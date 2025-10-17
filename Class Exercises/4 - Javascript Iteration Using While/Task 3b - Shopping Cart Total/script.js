3c20d19408156828227193e726cfc14f25ec58f3
IsaacWane <627iw13@canonslade.tbft.uk>
2025-10-17 11:20:22 +0000

    changes

--- Class Exercises/4 - Javascript Iteration Using While/Task 3b - Shopping Cart Total/script.js
+++ Class Exercises/4 - Javascript Iteration Using While/Task 3b - Shopping Cart Total/script.js
@@ -1,2 +1,6 @@
 // Add your event listener and while loop code here
+document.getElementById('addBtn').addEventListener('click', function () {
+
+
+});
 // When the button is clicked, use a while loop to total prices and count items until 0 is entered
